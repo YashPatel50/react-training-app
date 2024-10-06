74d48d7c602bc1f57fb5bd4832330d4edea198ba
YashPatel50 <patelshay8@gmail.com>
2024-10-06 15:32:05 -0400

    Created basic css file for List Group component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import Alert from "./Components/Alert";
 import FunButton from "./Components/FunButton";
+import ListGroup from "./Components/ListGroup";
 
 function App() {
-  //let items = ["Chicken Wrap", "Chicken Nuggets", "French Fries", "Coffee"];
+  let items = ["Chicken Wrap", "Chicken Nuggets", "French Fries", "Coffee"];
 
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
-  const [showAlert, setShowAlert] = useState(false);
+  //const [showAlert, setShowAlert] = useState(false);
 
   return (
     // 1. Using List Groups
-    // <ListGroup
-    //   items={items}
-    //   heading="Food Name"
-    //   onSelectItem={handleSelectItem}
-    // ></ListGroup>
+    <ListGroup
+      items={items}
+      heading="Food Name"
+      onSelectItem={handleSelectItem}
+    ></ListGroup>
 
     // 2. Using Alerts for children
     // <Alert>
@@ -25,20 +26,20 @@ function App() {
     // </Alert>
 
     // 3. Button Testing
-    <>
-      {showAlert && (
-        <Alert onClose={() => setShowAlert(false)}> My Alert</Alert>
-      )}
-      <FunButton
-        onClick={() => {
-          console.log("Clicked");
-          setShowAlert(true);
-        }}
-        color="primary"
-      >
-        Fun Button
-      </FunButton>
-    </>
+    // <>
+    //   {showAlert && (
+    //     <Alert onClose={() => setShowAlert(false)}> My Alert</Alert>
+    //   )}
+    //   <FunButton
+    //     onClick={() => {
+    //       console.log("Clicked");
+    //       setShowAlert(true);
+    //     }}
+    //     color="primary"
+    //   >
+    //     Fun Button
+    //   </FunButton>
+    // </>
   );
 }
 
