f922d126fa6beb213c598167335c38fc0224d53a
YashPatel50 <patelshay8@gmail.com>
2024-10-16 18:33:47 -0400

    Practiced Fetching Data

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from "react";
 import ProductList from "./Components/ProductList";
+import axios from "axios";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
+interface User {
+  id: number;
+  name: string;
+}
 
 const App3 = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
 
-    //This should 'undo' what effect does
-    //This is clean up
-    return () => disconnect();
-  });
-
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App3;
