e8eb62b5f1add2d66717a24c000af6a53c7eff78
YashPatel50 <patelshay8@gmail.com>
2024-10-16 18:16:36 -0400

    havinf useEffect use only when state changes

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+import ProductList from "./Components/ProductList";
 
 const App3 = () => {
-  const ref = useRef<HTMLInputElement>(null);
-
-  //after render
-  useEffect(() => {
-    //Side effect
-    if (ref.current) ref.current.focus();
-  });
-
-  useEffect(() => {
-    document.title = "My App";
-  });
+  const [category, setCategory] = useState("");
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select
+        className="form-select"
+        onChange={(event) => {
+          setCategory(event.target.value);
+        }}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category}></ProductList>
     </div>
   );
 };
--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-const ProductList = () => {
+const ProductList = ({ category }: { category: string }) => {
   const [products, setProducts] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log("Fetching Products");
+    console.log("Fetching Products in", category);
     setProducts(["Clothing", "HouseHold"]);
-  });
+  }, [category]);
   return <div>ProductList</div>;
 };
 
