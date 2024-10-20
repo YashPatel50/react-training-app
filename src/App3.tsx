2dfa1de147d8fea229671ff4422f9a06facfdf7c
YashPatel50 <patelshay8@gmail.com>
2024-10-20 16:36:53 -0400

    Handle error

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import ProductList from "./Components/ProductList";
 import axios from "axios";
 
 interface User {
@@ -9,15 +8,18 @@ interface User {
 
 const App3 = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => setUsers(res.data));
+      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
     <div>
+      {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
