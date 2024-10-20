5176ad3db313af5c153c82de1fa0782abf199148
YashPatel50 <patelshay8@gmail.com>
2024-10-20 17:01:36 -0400

    added a spinner if loading from server

--- src/App3.tsx
+++ src/App3.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -9,19 +9,33 @@ interface User {
 const App3 = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
-  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => setUsers(res.data))
-      .catch((err) => setError(err.message));
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setError("");
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
   }, []);
 
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
-      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
